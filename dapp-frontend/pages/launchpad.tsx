34e39e4938e4d7c97abfe5d6a73d8020d4680a39
promiselxg <okeydeede@gmail.com>
2023-04-25 16:04:18 +0100

    added luanchpad screen

--- dapp-frontend/pages/launchpad.tsx
+++ dapp-frontend/pages/launchpad.tsx
@@ -10,7 +10,7 @@ export default function Launchpad() {
         <title>Vefi Dapps | Launchpad</title>
       </Head>
       <div className="container mx-auto">
-        <section className="flex items-center h-[80vh] w-full ">
+        <section className="flex items-center h-[80vh] w-full relative">
           <div className="flex flex-col max-w-[550px]">
             <div className="mb-5">
               <h1 className="font-Syne text-[48px] text-white font-[700] leading-[58px]">Launch a successful IDO with ease-all in one place.</h1>
@@ -21,13 +21,11 @@ export default function Launchpad() {
               </button>
             </div>
           </div>
-
-          <div className="right"></div>
         </section>
         <section className="flex flex-col w-full container h-fit mx-auto pb-14">
           <h1 className="text-white font-Kinn font-[700] leading-[27px] pb-10 text-[24px]">What we offer</h1>
-          <div className="grid grid-cols-3 gap-2">
-            <div className="flex flex-col text-center justify-center rounded-[10px] shadow p-8 bg-lightBlue min-h-[200px] max-w-sm">
+          <div className="grid grid-cols-3 gap-5">
+            <div className="flex flex-col text-center justify-center rounded-[10px] shadow p-8 bg-lightBlue min-h-[200px] ">
               <h1 className="font-Kinn font-[700] text-[16px] leading-[16px] text-white">Secure Assets</h1>
               <p className="py-5 text-[#D8D8D8] font-Poppins text-[12px]">
                 Protect your investments with our secure IDO launchpad platform for hassle-free asset management.
@@ -36,7 +34,7 @@ export default function Launchpad() {
                 <Image src="/images/secure_assets.png" width={150} height={150} alt="secure assets" className=" object-contain" />
               </div>
             </div>
-            <div className="flex flex-col text-center justify-center rounded-[10px] shadow p-8 bg-lightBlue min-h-[200px] max-w-sm">
+            <div className="flex flex-col text-center justify-center rounded-[10px] shadow p-8 bg-lightBlue min-h-[200px] ">
               <h1 className="font-Kinn font-[700] text-[16px] leading-[16px] text-white">Free IDO marketing</h1>
               <p className="py-5 text-[#D8D8D8] font-Poppins text-[12px]">
                 With a robust presence of over 200k members across various social media platforms, we can confidently amplify your reach.
@@ -45,7 +43,7 @@ export default function Launchpad() {
                 <Image src="/images/ido_marketing.png" width={150} height={150} alt="Free IDO marketing" className=" object-contain" />
               </div>
             </div>
-            <div className="flex flex-col text-center justify-center rounded-[10px] shadow p-8 bg-lightBlue min-h-[200px] max-w-sm">
+            <div className="flex flex-col text-center justify-center rounded-[10px] shadow p-8 bg-lightBlue min-h-[200px] ">
               <h1 className="font-Kinn font-[700] text-[16px] leading-[16px] text-white">Advice and support</h1>
               <p className="py-5 text-[#D8D8D8] font-Poppins text-[12px]">
                 We provide industry-tested guidance to projects in maximizing the benefits of their IDO, and achieving project success.
@@ -59,7 +57,7 @@ export default function Launchpad() {
         <section className="flex flex-col h-fit my-10 ">
           <div className="container mx-auto ">
             <h1 className="max-w-[450px] text-white font-Syne font-[700] text-[32px] py-8">Explore live IDO projects on our launchpad</h1>
-            <div className="grid grid-cols-3 gap-2">
+            <div className="grid grid-cols-3 gap-3">
               <LaunchPadCard />
               <LaunchPadCard />
               <LaunchPadCard />
