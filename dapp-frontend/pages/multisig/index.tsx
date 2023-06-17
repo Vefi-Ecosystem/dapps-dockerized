c023d644357b4410335db62c99778b6933321116
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-06-17 21:41:48 +0100

    add: multisig page

---
+++ dapp-frontend/pages/multisig/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+
+const MultiSig = () => {
+    return (  
+        <>
+            <Head>
+                <title>VeFi Dapps | MultiSig</title>
+            </Head>
+
+            <div className="container">
+                
+            </div>
+        </>
+    );
+}
+ 
+export default MultiSig;
