12b865feb9e87a5900c6c63904020a36b87ecfae
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-06-21 17:28:42 +0100

    add: added widget to bridge page

---
+++ dapp-frontend/ui/Widget/index.tsx
@@ -0,0 +1,11 @@
+const StargateWidget = () => {
+    
+
+    return (
+        <div className="widget_parent">
+            <stargate-widget theme="dark" partnerId="2" feeCollector="0xc13b65f7c53Cd6db2EA205a4b574b4a0858720A6" tenthBps="10" />
+        </div>
+    );
+};
+
+export default StargateWidget;
