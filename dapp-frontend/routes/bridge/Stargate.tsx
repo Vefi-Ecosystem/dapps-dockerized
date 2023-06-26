1b975ccd294c43e6285ea2258ea609871e00dffc
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-06-26 14:06:34 +0100

    add : set up stargate

---
+++ dapp-frontend/routes/bridge/Stargate.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+import type { NextPage } from 'next';
+import StargateWidget from '../../ui/Widget'
+
+
+const Bridge: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>VeFi DApps | Bridge</title>
+      </Head>
+      <div className="container mx-auto w-[95%] md:w-2/6  py-20 ">
+        <StargateWidget />
+      </div>
+    </>
+  );
+}
+
+export default Bridge;
